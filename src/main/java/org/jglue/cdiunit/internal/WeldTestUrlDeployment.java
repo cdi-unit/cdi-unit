fccb17048bafb4b88335eb14a76102ad6e873d59
bryncooke <bryncooke@gmail.com>
2013-04-09 14:49:58 +0100

    Fix automatic pickup of beans.xml in WEB-INF directory

--- src/main/java/org/jglue/cdiunit/internal/WeldTestUrlDeployment.java
+++ src/main/java/org/jglue/cdiunit/internal/WeldTestUrlDeployment.java
@@ -62,196 +62,211 @@ import org.jglue.cdiunit.ProducesAlternative;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.html.UrlCssBuilder;
+
 public class WeldTestUrlDeployment extends AbstractWeldSEDeployment {
-	private final BeanDeploymentArchive _beanDeploymentArchive;
-	private Collection<Metadata<Extension>> _extensions = new ArrayList<Metadata<Extension>>();
-	private static Logger log = LoggerFactory.getLogger(WeldTestUrlDeployment.class);
-	private Set<URL> cdiClasspathEntries = new HashSet<URL>();
+    private final BeanDeploymentArchive _beanDeploymentArchive;
+    private Collection<Metadata<Extension>> _extensions = new ArrayList<Metadata<Extension>>();
+    private static Logger log = LoggerFactory.getLogger(WeldTestUrlDeployment.class);
+    private Set<URL> cdiClasspathEntries = new HashSet<URL>();
 
-	public WeldTestUrlDeployment(ResourceLoader resourceLoader, Bootstrap bootstrap, Class<?> testClass) throws IOException {
-		super(bootstrap);
+    public WeldTestUrlDeployment(ResourceLoader resourceLoader, Bootstrap bootstrap, Class<?> testClass) throws IOException {
+        super(bootstrap);
 
-		populateCdiClasspathSet();
-		// BeanDeploymentArchive archive = new URLScanner(resourceLoader,
-		// bootstrap, RESOURCES).scan();
-		BeansXml beansXml = new BeansXmlImpl(new ArrayList<Metadata<String>>(), new ArrayList<Metadata<String>>(), new ArrayList<Metadata<String>>(), new ArrayList<Metadata<String>>(),
-				Scanning.EMPTY_SCANNING);
-		Set<String> discoveredClasses = new LinkedHashSet<String>();
-		Set<String> alternatives = new HashSet<String>();
-		discoveredClasses.add(testClass.getName());
-		Set<Class<?>> classesToProcess = new LinkedHashSet<Class<?>>();
-		Set<Class<?>> classesProcessed = new HashSet<Class<?>>();
+        populateCdiClasspathSet();
+        // BeanDeploymentArchive archive = new URLScanner(resourceLoader,
+        // bootstrap, RESOURCES).scan();
+        BeansXml beansXml = new BeansXmlImpl(new ArrayList<Metadata<String>>(), new ArrayList<Metadata<String>>(), new ArrayList<Metadata<String>>(),
+                new ArrayList<Metadata<String>>(), Scanning.EMPTY_SCANNING);
+        Set<String> discoveredClasses = new LinkedHashSet<String>();
+        Set<String> alternatives = new HashSet<String>();
+        discoveredClasses.add(testClass.getName());
+        Set<Class<?>> classesToProcess = new LinkedHashSet<Class<?>>();
+        Set<Class<?>> classesProcessed = new HashSet<Class<?>>();
 
-		classesToProcess.add(testClass);
-		_extensions.add(new MetadataImpl<Extension>(new TestScopeExtension(testClass), TestScopeExtension.class.getName()));
-		try {
-			Class.forName("javax.servlet.http.HttpServletRequest");
-			classesToProcess.add(InRequestInterceptor.class);
-			classesToProcess.add(InSessionInterceptor.class);
-			classesToProcess.add(InConversationInterceptor.class);
-		} catch (ClassNotFoundException e) {
-		}
-		while (!classesToProcess.isEmpty()) {
-			Class<?> c = classesToProcess.iterator().next();
-			if (isCdiClass(c) && !classesProcessed.contains(c) && !c.isPrimitive()) {
-				classesProcessed.add(c);
-				discoveredClasses.add(c.getName());
-				if (Extension.class.isAssignableFrom(c)) {
-					try {
-						_extensions.add(new MetadataImpl<Extension>((Extension) c.newInstance(), c.getName()));
-					} catch (Exception e) {
-						throw new RuntimeException(e);
-					}
-				}
-				if (c.isAnnotationPresent(Interceptor.class)) {
-					beansXml.getEnabledInterceptors().add(new MetadataImpl<String>(c.getName(), c.getName()));
-				}
+        classesToProcess.add(testClass);
+        _extensions.add(new MetadataImpl<Extension>(new TestScopeExtension(testClass), TestScopeExtension.class.getName()));
+        try {
+            Class.forName("javax.servlet.http.HttpServletRequest");
+            classesToProcess.add(InRequestInterceptor.class);
+            classesToProcess.add(InSessionInterceptor.class);
+            classesToProcess.add(InConversationInterceptor.class);
+        } catch (ClassNotFoundException e) {
+        }
+        while (!classesToProcess.isEmpty()) {
+            Class<?> c = classesToProcess.iterator().next();
+            if(c.getName().endsWith("Identity")) {
+                System.out.println("BReak");
+            }
+            if (isCdiClass(c) && !classesProcessed.contains(c) && !c.isPrimitive()) {
+                classesProcessed.add(c);
+                discoveredClasses.add(c.getName());
+                if (Extension.class.isAssignableFrom(c)) {
+                    try {
+                        _extensions.add(new MetadataImpl<Extension>((Extension) c.newInstance(), c.getName()));
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                if (c.isAnnotationPresent(Interceptor.class)) {
+                    beansXml.getEnabledInterceptors().add(new MetadataImpl<String>(c.getName(), c.getName()));
+                }
 
-				if (isAlternativeStereotype(c)) {
-					beansXml.getEnabledAlternativeStereotypes().add(new MetadataImpl<String>(c.getName(), c.getName()));
+                if (isAlternativeStereotype(c)) {
+                    beansXml.getEnabledAlternativeStereotypes().add(new MetadataImpl<String>(c.getName(), c.getName()));
 
-				}
+                }
 
-				AdditionalClasses additionalClasses = c.getAnnotation(AdditionalClasses.class);
-				if (additionalClasses != null) {
-					for (Class<?> supportClass : additionalClasses.value()) {
-						classesToProcess.add(supportClass);
-					}
-					for (String lateBound : additionalClasses.late()) {
-						try {
-							Class<?> clazz = Class.forName(lateBound);
-							classesToProcess.add(clazz);
-						} catch (ClassNotFoundException e) {
-							throw new RuntimeException(e);
-						}
+                AdditionalClasses additionalClasses = c.getAnnotation(AdditionalClasses.class);
+                if (additionalClasses != null) {
+                    for (Class<?> supportClass : additionalClasses.value()) {
+                        classesToProcess.add(supportClass);
+                    }
+                    for (String lateBound : additionalClasses.late()) {
+                        try {
+                            Class<?> clazz = Class.forName(lateBound);
+                            classesToProcess.add(clazz);
+                        } catch (ClassNotFoundException e) {
+                            throw new RuntimeException(e);
+                        }
 
-					}
-				}
+                    }
+                }
 
-				ActivatedAlternatives alternativeClasses = c.getAnnotation(ActivatedAlternatives.class);
-				if (alternativeClasses != null) {
-					for (Class<?> alternativeClass : alternativeClasses.value()) {
-						classesToProcess.add(alternativeClass);
+                ActivatedAlternatives alternativeClasses = c.getAnnotation(ActivatedAlternatives.class);
+                if (alternativeClasses != null) {
+                    for (Class<?> alternativeClass : alternativeClasses.value()) {
+                        classesToProcess.add(alternativeClass);
 
-						if (!isAlternativeStereotype(alternativeClass)) {
-							alternatives.add(alternativeClass.getName());
-						}
-					}
-				}
+                        if (!isAlternativeStereotype(alternativeClass)) {
+                            alternatives.add(alternativeClass.getName());
+                        }
+                    }
+                }
 
-				Class<?> superClass = c.getSuperclass();
-				if (superClass != null && superClass != Object.class) {
-					classesToProcess.add(superClass);
-				}
+                Class<?> superClass = c.getSuperclass();
+                if (superClass != null && superClass != Object.class) {
+                    classesToProcess.add(superClass);
+                }
 
-				for (Field field : c.getDeclaredFields()) {
-					if (field.isAnnotationPresent(Inject.class)) {
-						Class<?> type = field.getType();
-						classesToProcess.add(type);
-					}
-					if (field.getType().equals(Provider.class)) {
-						ParameterizedType type = (ParameterizedType) field.getGenericType();
-						classesToProcess.add((Class<?>) type.getActualTypeArguments()[0]);
-					}
-				}
-				for (Method method : c.getDeclaredMethods()) {
-					if (method.isAnnotationPresent(Inject.class)) {
-						for (Class<?> param : method.getParameterTypes()) {
-							classesToProcess.add(param);
-						}
-					}
-				}
-			}
-			classesToProcess.remove(c);
-		}
+                for (Field field : c.getDeclaredFields()) {
+                    if (field.isAnnotationPresent(Inject.class)) {
+                        Class<?> type = field.getType();
+                        classesToProcess.add(type);
+                    }
+                    if (field.getType().equals(Provider.class)) {
+                        ParameterizedType type = (ParameterizedType) field.getGenericType();
+                        classesToProcess.add((Class<?>) type.getActualTypeArguments()[0]);
+                    }
+                }
+                for (Method method : c.getDeclaredMethods()) {
+                    if (method.isAnnotationPresent(Inject.class)) {
+                        for (Class<?> param : method.getParameterTypes()) {
+                            classesToProcess.add(param);
+                        }
+                    }
+                }
+            }
+            classesToProcess.remove(c);
+        }
 
-		beansXml.getEnabledAlternativeStereotypes().add(new MetadataImpl<String>(ProducesAlternative.class.getName(), ProducesAlternative.class.getName()));
+        beansXml.getEnabledAlternativeStereotypes().add(new MetadataImpl<String>(ProducesAlternative.class.getName(), ProducesAlternative.class.getName()));
 
-		for (String alternative : alternatives) {
-			beansXml.getEnabledAlternativeClasses().add(new MetadataImpl<String>(alternative, alternative));
-		}
+        for (String alternative : alternatives) {
+            beansXml.getEnabledAlternativeClasses().add(new MetadataImpl<String>(alternative, alternative));
+        }
 
-		try {
-			Class.forName("org.mockito.Mock");
-			_extensions.add(new MetadataImpl<Extension>(new MockExtension(), MockExtension.class.getName()));
-		} catch (ClassNotFoundException e) {
+        try {
+            Class.forName("org.mockito.Mock");
+            _extensions.add(new MetadataImpl<Extension>(new MockExtension(), MockExtension.class.getName()));
+        } catch (ClassNotFoundException e) {
 
-		}
+        }
 
-		_extensions.add(new MetadataImpl<Extension>(new WeldSEBeanRegistrant(), WeldSEBeanRegistrant.class.getName()));
+        _extensions.add(new MetadataImpl<Extension>(new WeldSEBeanRegistrant(), WeldSEBeanRegistrant.class.getName()));
 
-		_beanDeploymentArchive = new ImmutableBeanDeploymentArchive("unitTest", discoveredClasses, beansXml);
-		_beanDeploymentArchive.getServices().add(ResourceLoader.class, resourceLoader);
-		log.debug("CDI-Unit discovered:");
-		for (String clazz : discoveredClasses) {
-			if (!clazz.startsWith("org.jglue.cdiunit.internal.")) {
-				log.debug(clazz);
-			}
-		}
+        _beanDeploymentArchive = new ImmutableBeanDeploymentArchive("unitTest", discoveredClasses, beansXml);
+        _beanDeploymentArchive.getServices().add(ResourceLoader.class, resourceLoader);
+        log.debug("CDI-Unit discovered:");
+        for (String clazz : discoveredClasses) {
+            if (!clazz.startsWith("org.jglue.cdiunit.internal.")) {
+                log.debug(clazz);
+            }
+        }
 
-	}
+    }
 
-	private void populateCdiClasspathSet() throws IOException {
-		ClassLoader classLoader = WeldTestUrlDeployment.class.getClassLoader();
-		List<URL> entries = new ArrayList<URL>(Arrays.asList(((URLClassLoader) classLoader).getURLs()));
+    private void populateCdiClasspathSet() throws IOException {
+        ClassLoader classLoader = WeldTestUrlDeployment.class.getClassLoader();
+        List<URL> entries = new ArrayList<URL>(Arrays.asList(((URLClassLoader) classLoader).getURLs()));
 
-		// If this is surefire we need to get the original claspath
-		JarInputStream firstEntry = new JarInputStream(entries.get(0).openStream());
-		Manifest manifest = firstEntry.getManifest();
-		if (manifest != null) {
-			String classpath = (String) manifest.getMainAttributes().get(Attributes.Name.CLASS_PATH);
-			if (classpath != null) {
-				String[] manifestEntries = classpath.split(" ?file:");
-				for (String entry : manifestEntries) {
-					if (entry.length() > 0) {
-						entries.add(new URL("file:" + entry));
-					}
-				}
-			}
+        // If this is surefire we need to get the original claspath
+        JarInputStream firstEntry = new JarInputStream(entries.get(0).openStream());
+        Manifest manifest = firstEntry.getManifest();
+        if (manifest != null) {
+            String classpath = (String) manifest.getMainAttributes().get(Attributes.Name.CLASS_PATH);
+            if (classpath != null) {
+                String[] manifestEntries = classpath.split(" ?file:");
+                for (String entry : manifestEntries) {
+                    if (entry.length() > 0) {
+                        entries.add(new URL("file:" + entry));
+                    }
+                }
+            }
 
-		}
-		firstEntry.close();
+        }
+        firstEntry.close();
 
-		for (URL url : entries) {
-			URLClassLoader cl = new URLClassLoader(new URL[] { url }, null);
-			try {
-				URL resource = cl.getResource("META-INF/beans.xml");
-				URL resource2 = cl.getResource("WEB-INF/beans.xml");
-				boolean testClasses = url.getFile().endsWith("test-classes/");
-				if (resource != null || resource2 != null || testClasses) {
-					cdiClasspathEntries.add(url);
-				}
-			} finally {
-				// cl.close();
-			}
-		}
+        for (URL url : entries) {
+            URLClassLoader cl = new URLClassLoader(new URL[] { url }, null);
+            try {
+                if (url.getFile().endsWith("/classes/")) {
+                    URL webInfBeans = new URL(url, "../../src/main/webapp/WEB-INF/beans.xml");
+                    try {
+                        webInfBeans.openConnection();
+                        cdiClasspathEntries.add(url);
+                    } catch (IOException e) {
 
-	}
+                    }
+                }
+                URL resource = cl.getResource("META-INF/beans.xml");
 
-	private boolean isCdiClass(Class<?> c) {
-		if (c.getProtectionDomain().getCodeSource() == null) {
-			return false;
-		}
-		URL location = c.getProtectionDomain().getCodeSource().getLocation();
-		boolean isCdi = cdiClasspathEntries.contains(location);
-		return isCdi;
+                boolean testClasses = url.getFile().endsWith("/test-classes/");
+                if (resource != null || testClasses) {
+                    cdiClasspathEntries.add(url);
+                }
 
-	}
+            } finally {
+                // cl.close();
+            }
+        }
 
-	private boolean isAlternativeStereotype(Class<?> c) {
-		return c.isAnnotationPresent(Stereotype.class) && c.isAnnotationPresent(Alternative.class);
-	}
+    }
 
-	@Override
-	public Iterable<Metadata<Extension>> getExtensions() {
-		return _extensions;
-	}
+    private boolean isCdiClass(Class<?> c) {
+        if (c.getProtectionDomain().getCodeSource() == null) {
+            return false;
+        }
+        URL location = c.getProtectionDomain().getCodeSource().getLocation();
+        boolean isCdi = cdiClasspathEntries.contains(location);
+        return isCdi;
 
-	public List<BeanDeploymentArchive> getBeanDeploymentArchives() {
-		return Collections.singletonList(_beanDeploymentArchive);
-	}
+    }
 
-	public BeanDeploymentArchive loadBeanDeploymentArchive(Class<?> beanClass) {
-		return _beanDeploymentArchive;
-	}
+    private boolean isAlternativeStereotype(Class<?> c) {
+        return c.isAnnotationPresent(Stereotype.class) && c.isAnnotationPresent(Alternative.class);
+    }
+
+    @Override
+    public Iterable<Metadata<Extension>> getExtensions() {
+        return _extensions;
+    }
+
+    public List<BeanDeploymentArchive> getBeanDeploymentArchives() {
+        return Collections.singletonList(_beanDeploymentArchive);
+    }
+
+    public BeanDeploymentArchive loadBeanDeploymentArchive(Class<?> beanClass) {
+        return _beanDeploymentArchive;
+    }
 }
