a535093cc723bec805b1bc5c55f9c5029e182b3c
bryncooke <bryncooke@gmail.com>
2013-10-18 11:09:21 +0100

    Added test for class in main directory

--- cdi-unit-tests-bare/src/test/java/org/jglue/cdiunit/TestCdiRunner.java
+++ cdi-unit-tests-bare/src/test/java/org/jglue/cdiunit/TestCdiRunner.java
@@ -4,18 +4,24 @@ import javax.inject.Inject;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.cdiunit.NonTestClass;
 import org.junit.runner.RunWith;
 
 @RunWith(CdiRunner.class)
 @AdditionalClasses(AImplementation1.class)
 public class TestCdiRunner {
 
+	@Inject
+	private NonTestClass nonTestClass;
+	
     @Inject
     AInterface a;
 
     @Test
     public void testStart() {
         Assert.assertNotNull(a);
+        Assert.assertNotNull(nonTestClass);
+        nonTestClass.a();
     }
 
 }
