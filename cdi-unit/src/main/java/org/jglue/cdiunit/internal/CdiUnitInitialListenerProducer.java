28bade74e13dc254aee8cc4070b5f5799d565db9
Sean Flanigan <sflaniga@redhat.com>
2017-07-11 12:06:56 +1000

    Replace CGLib with Javassist

--- cdi-unit/src/main/java/org/jglue/cdiunit/internal/CdiUnitInitialListenerProducer.java
+++ cdi-unit/src/main/java/org/jglue/cdiunit/internal/CdiUnitInitialListenerProducer.java
@@ -1,11 +1,9 @@
 package org.jglue.cdiunit.internal;
 
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.NoOp;
 
 import javax.enterprise.inject.Produces;
 
-import static java.lang.Thread.currentThread;
+import javassist.util.proxy.ProxyFactory;
 
 /**
  * Decides which version of CdiUnitInitialListener to use, based on the Weld version.
@@ -22,17 +20,15 @@ public class CdiUnitInitialListenerProducer
             classToUse = CdiUnitInitialListenerImpl11.class;
         } catch (NoClassDefFoundError e) {
             try {
-                // We use CGLib to create a class which implements CdiUnitInitialListener but extends the Weld 3.0
+                // We use javassist to create a class which implements CdiUnitInitialListener but extends the Weld 3.0
                 // class WeldInitialListener (inheriting all its method implementations) thus avoiding a compile-time
                 // dependency on Weld 3.0 (since it's not practical to have Weld 1/2 and Weld 3 on the classpath in a
                 // single module).
                 Class<?> weldListenerClass = Class.forName("org.jboss.weld.module.web.servlet.WeldInitialListener");
-                Enhancer enhancer = new Enhancer();
-                enhancer.setClassLoader(currentThread().getContextClassLoader());
-                enhancer.setSuperclass(weldListenerClass);
-                enhancer.setInterfaces(new Class[] { CdiUnitInitialListener.class } );
-                enhancer.setCallbackType(NoOp.class);
-                classToUse = enhancer.createClass();
+                ProxyFactory factory = new ProxyFactory();
+                factory.setSuperclass(weldListenerClass);
+                factory.setInterfaces(new Class[] { CdiUnitInitialListener.class } );
+                classToUse = factory.createClass();
             } catch (Exception e1) {
                 throw new RuntimeException(e1);
             }
