55518d0f289b5cf37e923f7cfa994302c1ee8908
Illya Kysil <ikysil@ikysil.name>
2020-05-17 18:21:38 +0200

    Refactor - use eager class lookup in DeltaspikeProxyDiscoveryExtension
    Alignment with the rest of the discovery extensions.

--- cdi-unit/src/main/java/org/jglue/cdiunit/internal/deltaspike/DeltaspikeProxyDiscoveryExtension.java
+++ cdi-unit/src/main/java/org/jglue/cdiunit/internal/deltaspike/DeltaspikeProxyDiscoveryExtension.java
@@ -1,38 +1,46 @@
 package org.jglue.cdiunit.internal.deltaspike;
 
 import org.apache.deltaspike.data.impl.RepositoryExtension;
+import org.jglue.cdiunit.internal.ClassLookup;
 import org.jglue.cdiunit.internal.DiscoveryExtension;
 
 import java.util.Collections;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class DeltaspikeProxyDiscoveryExtension implements DiscoveryExtension {
 
 	private static final String DELTASPIKE_17_18_ANCHOR_CLASS_NAME = "org.apache.deltaspike.proxy.impl.invocation.InterceptorLookup";
 	private static final String DELTASPIKE_19_ANCHOR_CLASS_NAME = "org.apache.deltaspike.proxy.spi.invocation.DeltaSpikeProxyInvocationHandler";
 
-	// We don't expect classpath to change between invocations
-	private static Class<?> proxyModuleAnchor = null;
+	/**
+	 * The non-null value here means that we have DeltaSpike 1.7.x or 1.8.x in the classpath.
+	 */
+	private final Class<?> deltaSpike_17x_18x_proxyModuleAnchor =
+		ClassLookup.INSTANCE.lookup(DELTASPIKE_17_18_ANCHOR_CLASS_NAME);
+
+	/**
+	 * The non-null value here means that we have DeltaSpike 1.9.x in the classpath.
+	 */
+	private final Class<?> deltaSpike_19x_proxyModuleAnchor =
+		ClassLookup.INSTANCE.lookup(DELTASPIKE_19_ANCHOR_CLASS_NAME);
+
+	private final Class<?> proxyModuleAnchor;
+
+	public DeltaspikeProxyDiscoveryExtension() {
+		proxyModuleAnchor = Stream.of(
+			deltaSpike_19x_proxyModuleAnchor,
+			deltaSpike_17x_18x_proxyModuleAnchor
+		)
+			.filter(Objects::nonNull)
+			.findFirst()
+			.orElse(null);
+	}
 
 	@Override
 	public void bootstrap(BootstrapDiscoveryContext bdc) {
-		// support for DeltaSpike 1.7.x and 1.8.x
-		discoverDeltaSpikeProxyBeans(DELTASPIKE_17_18_ANCHOR_CLASS_NAME);
-		// support for DeltaSpike 1.9.x
-		discoverDeltaSpikeProxyBeans(DELTASPIKE_19_ANCHOR_CLASS_NAME);
 		if (proxyModuleAnchor != null) {
-			// only versions listed above need additional discovery
 			bdc.discoverClass(this::discoverClass);
-		}
-	}
-
-	private void discoverDeltaSpikeProxyBeans(String anchorClassName) {
-		if (proxyModuleAnchor != null) {
-			return;
-		}
-		try {
-			proxyModuleAnchor = Class.forName(anchorClassName);
-		} catch (ClassNotFoundException e) {
-			// version not detected
 		}
 	}
 
