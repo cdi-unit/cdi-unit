18d6f4d6123a7fe857f80f9d0c2d1362190b676b
Illya Kysil <ikysil@ikysil.name>
2020-05-25 23:26:51 +0200

    Improve detection and reporting of the invalid @Rule annotation usage - issue #154

---
+++ cdi-unit/src/main/java/org/jglue/cdiunit/internal/junit/InvalidRuleFieldUsageException.java
@@ -0,0 +1,9 @@
+package org.jglue.cdiunit.internal.junit;
+
+public class InvalidRuleFieldUsageException extends RuntimeException {
+
+	public InvalidRuleFieldUsageException(String message) {
+		super(message);
+	}
+
+}
