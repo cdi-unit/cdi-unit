f71e0b4bed368a86d3b60e6122d3f261773287cc
Illya Kysil <ikysil@users.noreply.github.com>
2020-08-18 10:20:26 +0200

    Add latest Weld release 3.1.5.Final (#183)

--- travisci/weld-v3.1.sh
+++ travisci/weld-v3.1.sh
@@ -3,6 +3,7 @@
 # Current strategy is, for each supported minor version of Weld, to test one early point release plus
 # the most recent point release, plus some point releases which caused problems.
 WELD_VERSIONS=(
+  "3.1.5.Final"
   "3.1.4.Final"
   "3.1.0.Final"
 )
