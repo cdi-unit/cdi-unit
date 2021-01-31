0487dcbde9547ce75f9db756856a909eeaf8528f
Illya Kysil <ikysil@ikysil.name>
2021-01-31 22:08:12 +0100

    (ci): Add latest minor versions for support major versions of Weld

--- travisci/weld-v2.2.sh
+++ travisci/weld-v2.2.sh
@@ -3,7 +3,7 @@
 # Current strategy is, for each supported minor version of Weld, to test one early point release plus
 # the most recent point release, plus some point releases which caused problems.
 WELD_VERSIONS=(
-  # "2.2.16.SP1"
+  "2.2.16.SP1"
   "2.2.13.Final"
   "2.2.9.Final" #  2.2.7, 2.2.8 not supported, but 2.2.9 is
   "2.2.0.Final"
--- travisci/weld-v2.4.sh
+++ travisci/weld-v2.4.sh
@@ -3,8 +3,8 @@
 # Current strategy is, for each supported minor version of Weld, to test one early point release plus
 # the most recent point release, plus some point releases which caused problems.
 WELD_VERSIONS=(
-  # "2.4.8.Final"
-  "2.4.4.Final"
+  "2.4.8.Final"
+  "2.4.5.Final"
   "2.4.2.Final" # BeansXmlImpl was changed in 2.4.2
   "2.4.0.Final"
 )
