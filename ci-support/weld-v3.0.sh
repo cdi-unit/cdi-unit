4475953e494e9dc17c9aa3b93037804136232bc7
Illya Kysil <ikysil@ikysil.name>
2024-05-27 18:19:13 +0200

    chore: Add latest supported Weld releases to the test matrix

--- ci-support/weld-v3.0.sh
+++ ci-support/weld-v3.0.sh
@@ -3,6 +3,7 @@
 # Current strategy is, for each supported minor version of Weld, to test one early point release plus
 # the most recent point release, plus some point releases which caused problems.
 WELD_VERSIONS=(
+  "3.0.6.Final"
   "3.0.5.Final"
   "3.0.4.Final"
   "3.0.0.Final"
