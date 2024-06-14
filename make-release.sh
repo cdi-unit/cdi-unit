a58a06c06100615ec7454a020455f4d95534b145
Illya Kysil <ikysil@ikysil.name>
2024-06-14 14:47:37 +0200

    chore: Update release script

--- make-release.sh
+++ make-release.sh
@@ -11,8 +11,8 @@ if ./mvnw release:prepare release:perform ${releaseOptions}; then
   # NB This line will fail the script if the 2nd last commit is not a tag
   tag=$(git describe --exact-match --abbrev=0 HEAD~1)
   echo "Maven release deployment succeeded."
-  echo "Please visit Sonatype OSS to close, release and drop the staging repository:"
-  echo "      https://oss.sonatype.org/#stagingRepositories"
+  echo "Please visit the Central Portal to drop or publish the artifacts:"
+  echo "      https://central.sonatype.com/publishing/deployments"
   echo
   # NB: Maven Central validation might fail
   echo "Note: if the release fails, you should run this to clean up:"
@@ -35,8 +35,8 @@ else
   # Assuming that the tag was found, it should be safe to reset the branch
   git reset --hard HEAD~2
 
-  echo "You may need to delete the staging repository in Sonatype OSS."
-  echo "      https://oss.sonatype.org/#stagingRepositories"
+  echo "You may need to drop the deployment at the Central Portal."
+  echo "      https://central.sonatype.com/publishing/deployments"
   echo
   echo "You should probably run this before trying again: ./mvnw release:clean"
 fi
