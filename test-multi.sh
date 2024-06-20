2827275912aa1283f362def97641341e5c05dd50
Illya Kysil <ikysil@ikysil.name>
2024-06-20 23:04:51 +0200

    build: Remove obsolete profiles for Weld 1, 2, and 3

--- test-multi.sh
+++ test-multi.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -ex
 
-mvn clean install -Dweld.test.version=3.0.0.Final
-mvn verify -pl \!cdi-unit -Dweld.test.version=2.4.2.Final -Dweld.test.1or2
-mvn verify -pl \!cdi-unit -Dweld.test.version=2.4.0.Final -Dweld.test.1or2
-mvn verify -pl \!cdi-unit -Dweld.test.version=2.3.5.Final -Dweld.test.1or2
-mvn verify -pl \!cdi-unit -Dweld.test.version=2.2.13.Final -Dweld.test.1or2
-mvn verify -pl \!cdi-unit -Dweld.test.version=2.2.9.Final -Dweld.test.1or2
-mvn verify -pl \!cdi-unit -Dweld.test.version=2.1.2.Final -Dweld.test.1or2
-mvn verify -pl \!cdi-unit -Dweld.test.version=2.0.4.Final -Dweld.test.1or2
-mvn verify -pl \!cdi-unit -Dweld.test.version=1.1.14.Final -Dweld.test.1or2
+mvn clean install
+mvn verify -pl \!cdi-unit -Dversion.weld.test=5.0.1.Final
+mvn verify -pl \!cdi-unit -Dversion.weld.test=5.1.2.Final
