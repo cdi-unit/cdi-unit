79977de4d9801dae42f058f7597e6b9bb3fa24eb
Illya Kysil <ikysil@ikysil.name>
2024-06-20 20:51:46 +0200

    ci: Update Weld test matrix

--- ci-support/weld-v5.0.sh
+++ ci-support/weld-v5.0.sh
@@ -3,7 +3,5 @@
 # Current strategy is, for each supported minor version of Weld, to test one early point release plus
 # the most recent point release, plus some point releases which caused problems.
 WELD_VERSIONS=(
-  "2.3.5.Final"
-  "2.3.2.Final" # firing of @Initialized(ApplicationScoped.class) was different in 2.3.2
-  "2.3.0.Final"
+  "5.0.1.Final"
 )
--- ci-support/weld-v5.1.sh
+++ ci-support/weld-v5.1.sh
@@ -3,7 +3,5 @@
 # Current strategy is, for each supported minor version of Weld, to test one early point release plus
 # the most recent point release, plus some point releases which caused problems.
 WELD_VERSIONS=(
-  "2.0.5.Final"
-  "2.0.4.Final"
-  "2.0.1.Final"
+  "5.1.2.Final"
 )
