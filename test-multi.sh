253c7ab8554e6636aca8166dcad93fb398bcfbc7
Illya Kysil <ikysil@ikysil.name>
2024-06-22 22:36:38 +0200

    build: Update Maven wrapper to the latest release 3.9.8

--- test-multi.sh
+++ test-multi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
-mvn clean install
-mvn verify -pl \!cdi-unit -Dversion.weld.test=5.0.1.Final
-mvn verify -pl \!cdi-unit -Dversion.weld.test=5.1.2.Final
+./mvnw clean install
+./mvnw verify -pl \!cdi-unit -Dversion.weld.test=5.0.1.Final
+./mvnw verify -pl \!cdi-unit -Dversion.weld.test=5.1.2.Final
