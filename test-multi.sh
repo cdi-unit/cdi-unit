b656a4d77eab7fcc519022b073f96074ebe37c6b
Illya Kysil <ikysil@ikysil.name>
2024-07-13 22:43:05 +0200

    chore: Update test-multi after introducing cdi-unit-core

--- test-multi.sh
+++ test-multi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 
 ./mvnw clean install
-./mvnw verify -pl \!cdi-unit -Dversion.weld.test=5.0.1.Final
-./mvnw verify -pl \!cdi-unit -Dversion.weld.test=5.1.2.Final
+./mvnw verify -pl \!cdi-unit-core -Dversion.weld.test=5.0.1.Final
+./mvnw verify -pl \!cdi-unit-core -Dversion.weld.test=5.1.2.Final
