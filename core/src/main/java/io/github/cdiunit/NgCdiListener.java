049cafd062b231b8f35ec99c0c2415c018e645bc
Illya Kysil <ikysil@ikysil.name>
2024-07-31 13:54:05 +0200

    refact: Extract reusable NgInvocationContext

--- core/src/main/java/io/github/cdiunit/NgCdiListener.java
+++ core/src/main/java/io/github/cdiunit/NgCdiListener.java
@@ -1,30 +1,28 @@
 package io.github.cdiunit;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import java.util.*;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import jakarta.enterprise.context.spi.CreationalContext;
-import jakarta.enterprise.inject.spi.*;
-import jakarta.interceptor.InvocationContext;
+import jakarta.enterprise.inject.spi.AnnotatedType;
+import jakarta.enterprise.inject.spi.BeanManager;
+import jakarta.enterprise.inject.spi.InjectionTarget;
 
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 import org.testng.IHookCallBack;
 import org.testng.IHookable;
-import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
 
 import io.github.cdiunit.internal.ExceptionUtils;
 import io.github.cdiunit.internal.TestConfiguration;
 import io.github.cdiunit.internal.WeldHelper;
 import io.github.cdiunit.internal.activatescopes.ScopesHelper;
+import io.github.cdiunit.internal.testng.NgInvocationContext;
 
-public class NgCdiListener implements IHookable, IInvokedMethodListener {
+public class NgCdiListener implements IHookable {
 
     private Weld weld;
     private WeldContainer container;
@@ -87,109 +85,6 @@ public class NgCdiListener implements IHookable, IInvokedMethodListener {
                 throw ExceptionUtils.asRuntimeException(e);
             }
         }
-    }
-
-    static class NgInvocationContext<H> implements InvocationContext {
-
-        private final IHookCallBack callBack;
-        private final ITestResult testResult;
-        private final Method method;
-
-        private List<Interceptor<?>> interceptors = List.of();
-        private Map<String, Object> contextData;
-        private BeanManager beanManager;
-
-        private int interceptorIndex;
-
-        public NgInvocationContext(IHookCallBack callBack, ITestResult testResult) {
-            this.callBack = callBack;
-            this.testResult = testResult;
-            this.method = testResult.getMethod().getConstructorOrMethod().getMethod();
-        }
-
-        void configure(BeanManager beanManager) {
-            if (method == null) {
-                return;
-            }
-            var bindings = Arrays.stream(method.getAnnotations())
-                    .filter(o -> beanManager.isInterceptorBinding(o.annotationType()))
-                    .toArray(Annotation[]::new);
-            if (bindings.length == 0) {
-                // skip interceptors since WELD-001311: Interceptor bindings list cannot be empty
-                return;
-            }
-            this.beanManager = beanManager;
-            this.interceptors = beanManager.resolveInterceptors(InterceptionType.AROUND_INVOKE, bindings);
-        }
-
-        @Override
-        public Object getTarget() {
-            return testResult.getInstance();
-        }
-
-        @Override
-        public Object getTimer() {
-            return null;
-        }
-
-        @Override
-        public Method getMethod() {
-            return method;
-        }
-
-        @Override
-        public Constructor<?> getConstructor() {
-            return null;
-        }
-
-        @Override
-        public Object[] getParameters() {
-            return callBack.getParameters();
-        }
-
-        @Override
-        public void setParameters(Object[] params) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public Map<String, Object> getContextData() {
-            if (contextData == null) {
-                contextData = new HashMap<>();
-            }
-            return contextData;
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public Object proceed() throws Exception {
-            if (interceptors.size() > interceptorIndex) {
-                Interceptor<H> interceptor = null;
-                CreationalContext<H> creationalContext = null;
-                H interceptorInstance = null;
-
-                try {
-                    interceptor = (Interceptor<H>) interceptors.get(interceptorIndex++);
-                    creationalContext = beanManager.createCreationalContext(interceptor);
-                    interceptorInstance = interceptor.create(creationalContext);
-
-                    return interceptor.intercept(InterceptionType.AROUND_INVOKE, interceptorInstance, this);
-                } finally {
-                    if (creationalContext != null) {
-                        if (interceptorInstance != null && interceptor != null) {
-                            interceptor.destroy(interceptorInstance, creationalContext);
-                        }
-
-                        creationalContext.release();
-                    }
-                }
-            }
-
-            callBack.runTestMethod(testResult);
-            // test methods are void
-            return null;
-        }
-
     }
 
 }
