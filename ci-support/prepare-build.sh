bf50365d86bbd8ecb73c1e1ee10724dc17fbf9d5
Illya Kysil <ikysil@ikysil.name>
2024-08-03 16:16:47 +0200

    ci: Don't use caching provided by the setup-java action

--- ci-support/prepare-build.sh
+++ ci-support/prepare-build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-export MAVEN_ARGS="-V -B --settings .github/mvn-settings.xml"
+export MAVEN_ARGS="-V -B --settings .github/mvn-settings.xml --no-transfer-progress"
 
 ./mvnw -Dno-format dependency:go-offline
 ./mvnw -Dno-format -DskipTests package
