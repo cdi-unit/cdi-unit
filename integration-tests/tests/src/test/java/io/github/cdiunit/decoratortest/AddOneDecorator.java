fee874a6f0560016f7905438f160c64b1734e369
Illya Kysil <ikysil@ikysil.name>
2024-08-03 20:44:05 +0200

    chore: Add Apache 2 License Header

--- integration-tests/tests/src/test/java/io/github/cdiunit/decoratortest/AddOneDecorator.java
+++ integration-tests/tests/src/test/java/io/github/cdiunit/decoratortest/AddOneDecorator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.cdiunit.decoratortest;
 
 import jakarta.decorator.Decorator;
@@ -5,9 +20,6 @@ import jakarta.decorator.Delegate;
 import jakarta.enterprise.inject.Any;
 import jakarta.inject.Inject;
 
-/**
- * Created by pcasaes on 30/03/17.
- */
 @Decorator
 public abstract class AddOneDecorator implements DecoratedInterface {
     @Inject
