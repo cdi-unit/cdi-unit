016714ccc6cae93f5583dbe55b8e074b583ec2a6
Illya Kysil <ikysil@users.noreply.github.com>
2024-08-11 20:56:31 +0200

    chore: Align naming in DeltaSpike modules (#318)

---
+++ deltaspike/src/main/java/io/github/cdiunit/deltaspike/internal/DeltaspikeProxyDiscoveryExtension.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.cdiunit.deltaspike.internal;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import org.apache.deltaspike.data.impl.RepositoryExtension;
+
+import io.github.cdiunit.internal.ClassLookup;
+import io.github.cdiunit.internal.DiscoveryExtension;
+
+public class DeltaspikeProxyDiscoveryExtension implements DiscoveryExtension {
+
+    /**
+     * DeltaSpike 2.0.x proxies support
+     */
+    private static final String DELTASPIKE_20x_ANCHOR_CLASS_NAME = "org.apache.deltaspike.proxy.spi.invocation.DeltaSpikeProxyInvocationHandler";
+
+    private final Class<?> proxyModuleAnchor;
+
+    public DeltaspikeProxyDiscoveryExtension() {
+        proxyModuleAnchor = Stream.of(
+                ClassLookup.INSTANCE.lookup(DELTASPIKE_20x_ANCHOR_CLASS_NAME))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public void bootstrap(BootstrapDiscoveryContext bdc) {
+        if (proxyModuleAnchor != null) {
+            bdc.discoverClass(this::discoverClass);
+        }
+    }
+
+    private void discoverClass(Context context, Class<?> cls) {
+        if (cls != RepositoryExtension.class) {
+            return;
+        }
+        // scan proxy module only if RepositoryExtension was requested
+        context.scanBeanArchives(Collections.singleton(proxyModuleAnchor))
+                .forEach(context::processBean);
+    }
+
+}
---
+++ integration-tests/deltaspike-tests/src/test/java/io/github/cdiunit/deltaspike/tests/TestDeltaspikeTransactions.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2014 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.cdiunit.deltaspike.tests;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import org.apache.deltaspike.jpa.api.transaction.Transactional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.github.cdiunit.CdiRunner;
+import io.github.cdiunit.InRequestScope;
+import io.github.cdiunit.deltaspike.SupportDeltaspikeData;
+import io.github.cdiunit.deltaspike.SupportDeltaspikeJpa;
+
+@SupportDeltaspikeJpa
+@SupportDeltaspikeData
+@RunWith(CdiRunner.class)
+public class TestDeltaspikeTransactions {
+
+    @Inject
+    private TestEntityRepository er;
+    private EntityManagerFactory emf;
+
+    @PostConstruct
+    public void init() {
+        emf = Persistence
+                .createEntityManagerFactory("DefaultPersistenceUnit");
+    }
+
+    @Produces
+    //@RequestScoped
+    protected EntityManager createEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    @InRequestScope
+    @Transactional
+    @Test
+    public void test() {
+        TestEntity t = new TestEntity();
+        er.save(t);
+    }
+}
