3e63956618e469c970fc9534a9ea69e795aa56da
Illya Kysil <ikysil@ikysil.name>
2024-08-15 18:50:18 +0200

    ci: Test Spock with different Groovy versions

---
+++ ci-support/groovy-v4.0.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Current strategy is, for each supported minor version of Groovy, to test one early point release plus
+# the most recent point release, plus some point releases which caused problems.
+GROOVY_VERSION_COMPILE=4
+GROOVY_VERSIONS=(
+  "4.0.22"
+)
