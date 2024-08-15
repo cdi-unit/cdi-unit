a5417e747215f05cd55b9ca5f6ca80adc5966a91
Illya Kysil <ikysil@ikysil.name>
2024-08-15 21:46:08 +0200

    build: Minimal JavaDoc in servlet support packages
    To satisfy Maven Central requirements.

---
+++ support-servlet/src/main/java/io/github/cdiunit/servlet5/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Support classes for integrating the <em>CDI Unit</em> with the
+ * <a href="https://jakarta.ee/specifications/servlet/5.0/">Jakarta Servlet 5.0</a>.
+ *
+ * NOTE: there are no public classes in this package.
+ *
+ * @see <a href="https://jakarta.ee/specifications/servlet/5.0/">Jakarta Servlet 5.0</a>
+ */
+package io.github.cdiunit.servlet5;
---
+++ support-servlet6/src/main/java/io/github/cdiunit/servlet6/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Support classes for integrating the <em>CDI Unit</em> with the
+ * <a href="https://jakarta.ee/specifications/servlet/6.0/">Jakarta Servlet 6.0</a>.
+ *
+ * NOTE: there are no public classes in this package.
+ *
+ * @see <a href="https://jakarta.ee/specifications/servlet/6.0/">Jakarta Servlet 6.0</a>
+ */
+package io.github.cdiunit.servlet6;
