f46c36c6b5b7743a4250e943de8223b6cc2bc344
Illya Kysil <ikysil@ikysil.name>
2024-10-08 19:58:12 +0200

    tests: Move TestNonCDIClasses to core tests

---
+++ core/src/test/java/io/github/cdiunit/core/tests/TestNonCDIClasses.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2013 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.cdiunit.core.tests;
+
+import jakarta.enterprise.inject.spi.DeploymentException;
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+
+import ch.qos.logback.classic.filter.ThresholdFilter;
+import ch.qos.logback.core.filter.Filter;
+import io.github.cdiunit.AdditionalClasses;
+import io.github.cdiunit.internal.TestConfiguration;
+import io.github.cdiunit.internal.TestLifecycle;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class TestNonCDIClasses {
+
+    @AdditionalClasses(ThresholdFilter.class)
+    static class TestBean {
+
+        @Inject
+        private Filter foo;
+
+        private ThresholdFilter bar;
+
+    }
+
+    @Test
+    public void testNonCDIClassDiscovery() throws Throwable {
+        var testLifecycle = new TestLifecycle(new TestConfiguration(TestBean.class));
+
+        assertThatExceptionOfType(DeploymentException.class).isThrownBy(() -> {
+            TestBean bean = testLifecycle.createTest(null);
+        });
+
+        testLifecycle.shutdown();
+    }
+
+}
