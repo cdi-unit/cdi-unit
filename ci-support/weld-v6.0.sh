233f1f1dd6851717334c0a8542a03d87d026dfef
Illya Kysil <ikysil@ikysil.name>
2024-10-14 19:54:16 +0200

    ci: Add Weld 6 (6.0.0.Beta4) to test matrix)

---
+++ ci-support/weld-v6.0.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Current strategy is, for each supported minor version of Weld, to test one early point release plus
+# the most recent point release, plus some point releases which caused problems.
+WELD_VERSIONS=(
+  "6.0.0.Beta4"
+)
