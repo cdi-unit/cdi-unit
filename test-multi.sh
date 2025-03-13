baf62d36807be9f374c697e84e4b48f597cf9fe8
Illya Kysil <ikysil@ikysil.name>
2025-03-13 08:25:43 +0100

    deps: Test with latest releases of Weld 5.1.x and 6.0.x series

--- test-multi.sh
+++ test-multi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
 ./mvnw clean install
+./mvnw verify -f integration-tests -Dversion.weld.test=4.0.3.Final
 ./mvnw verify -f integration-tests -Dversion.weld.test=5.0.1.Final
-./mvnw verify -f integration-tests -Dversion.weld.test=5.1.2.Final
+./mvnw verify -f integration-tests -Dversion.weld.test=5.1.4.Final
+./mvnw verify -f integration-tests -Dversion.weld.test=6.0.1.Final
