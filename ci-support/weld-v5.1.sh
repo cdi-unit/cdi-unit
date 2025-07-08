950bfaa806126b6312d9e332db4c0512220d7368
Illya Kysil <ikysil@ikysil.name>
2025-07-08 08:17:27 +0200

    deps: Test with latest releases of Weld 5.1.x and 6.0.x series - 2025Q3

--- ci-support/weld-v5.1.sh
+++ ci-support/weld-v5.1.sh
@@ -3,6 +3,6 @@
 # Current strategy is, for each supported minor version of Weld, to test one early point release plus
 # the most recent point release, plus some point releases which caused problems.
 WELD_VERSIONS=(
-  "5.1.4.Final"
   "5.1.5.Final"
+  "5.1.6.Final"
 )
--- ci-support/weld-v6.0.sh
+++ ci-support/weld-v6.0.sh
@@ -3,7 +3,6 @@
 # Current strategy is, for each supported minor version of Weld, to test one early point release plus
 # the most recent point release, plus some point releases which caused problems.
 WELD_VERSIONS=(
-  "6.0.0.Final"
-  "6.0.1.Final"
   "6.0.2.Final"
+  "6.0.3.Final"
 )
