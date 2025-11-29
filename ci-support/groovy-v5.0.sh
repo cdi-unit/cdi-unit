dcaaf5c7252058cbd731edbd1a23611bb1d7d782
Illya Kysil <ikysil@ikysil.name>
2025-11-29 23:07:41 +0100

    build(deps): Support for Groovy 5

--- ci-support/groovy-v5.0.sh
+++ ci-support/groovy-v5.0.sh
@@ -2,9 +2,9 @@
 
 # Current strategy is, for each supported minor version of Groovy, to test one early point release plus
 # the most recent point release, plus some point releases which caused problems.
-GROOVY_MAVEN_ARGS="-Dspock.iKnowWhatImDoing.disableGroovyVersionCheck=true"
-GROOVY_VERSION_COMPILE=4
+GROOVY_VERSION_COMPILE=5
 GROOVY_VERSIONS=(
   "5.0.0"
   "5.0.1"
+  "5.0.2"
 )
