6bbdd1c60a3b13a04fdbb0c192d7395aae61f6fa
Illya Kysil <ikysil@ikysil.name>
2025-12-02 08:18:41 +0100

    tests(ci): Rework Spock test matrix

--- ci-support/groovy-v2.5-jdk17lts.sh
+++ ci-support/groovy-v2.5-jdk17lts.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+
 # Current strategy is, for each supported minor version of Groovy, to test one early point release plus
 # the most recent point release, plus some point releases which caused problems.
-GROOVY_VERSION_COMPILE=3
+GROOVY_VERSION_COMPILE=2.5
+# These versions support JDKs up to 17 LTS
 GROOVY_VERSIONS=(
-  "3.0.22"
-  "3.0.24"
-  "3.0.25"
+  "2.5.18"
+  "2.5.21"
+  "2.5.23"
 )
---
+++ ci-support/groovy-v4.0-jdk25lts.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Current strategy is, for each supported minor version of Groovy, to test one early point release plus
+# the most recent point release, plus some point releases which caused problems.
+GROOVY_VERSION_COMPILE=4
+# These versions support JDKs up to 25 LTS
+GROOVY_VERSIONS=(
+  "4.0.28"
+  "4.0.29"
+)
